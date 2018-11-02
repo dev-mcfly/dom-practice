11ea53ded48a0a36422c9bc5f6c22c6be8651245
Munir Mcfly <devmcfly04@gmail.com>
2018-11-01 19:54:44 -0700

    Used querySelector and getElementsByClassName to grab elements from the DOM

---
+++ app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// Changing the color of the box
+
+/* 
+const square = document.querySelector('.square');
+console.log(square)
+// let square = document.getElementsByClassName('square')
+square[0].style.backgroundColor = 'blue';
+*/
+
+// Changing the text in movie list
+/*
+const lis = document.querySelectorAll('.fav-movie');
+console.log(lis);
+for (let i = 0; i < lis.length; i++) {
+    console.log(lis[i])
+    lis[i].innerHTML += ' (new movie)'
+};
+*/
+
+
+
